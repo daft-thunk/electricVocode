bf0d8b4622a521c086ac9e27975366121e21389a
jakeBergs <jake.bergal@gmail.com>
2018-03-22 15:11:36 -0500

    finished user snippet view screen. still needs functionality in a lot of areas

--- app/components/Snippets.js
+++ app/components/Snippets.js
@@ -1,43 +1,62 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Table, Icon, Divider, Button } from 'antd';
+import { Table, Icon, Divider, Button, Modal, Popconfirm, message } from 'antd';
 import { Test } from './Test';
-import { fetchUserSnippets } from '../store/snippets'
+import { fetchUserSnippets } from '../store/snippets';
+import SnippetView from './SnippetView';
 
 class Snippets extends Component {
   constructor() {
-    super()
+    super();
 
   }
 
   componentDidMount() {
-    this.props.fetchSnippets(3)
+    this.props.fetchSnippets(this.props.userId);
   }
 
   render() {
+    console.log(this.props.snippets);
+    const remText = 'Remove this snippet?';
+    const confirm = () => {message.info('Snippet has been removed')}
     const columns = [{
       title: 'Command',
       dataIndex: 'command',
       key: 'command',
     },
     {
+      title: 'Code',
+      key: 'id',
+      render: (text, record) => (
+        <span>
+          <SnippetView code={record.code} command={record.command} />
+        </span>
+      )
+    },
+    {
       title: 'Actions',
       key: 'actions',
       render: (text, record) => (
         <span>
-          <Link to={`/snippet/${record.id}`} >View</Link>
+          {
+            this.props.userId === record.creatorId ?
+            <Button icon="edit" /> :
+            <Button icon="fork" />
+          }
           <Divider type="vertical" />
-          <Button><Icon type="plus-square" /></Button>
+          <Popconfirm title={remText} onConfirm={confirm} okText="Yes" cancelText="No" >
+            <Button type="danger" icon="close" />
+          </Popconfirm>
         </span>
       )
-    }]
+    }];
     return (
       <div>
         <h2>Manage Snippets</h2>
         <Table dataSource={this.props.snippets} columns={columns} />
       </div>
-    )
+    );
   }
 
 }
@@ -47,13 +66,14 @@ const mapDispatch = (dispatch) => {
     fetchSnippets(userId) {
       dispatch(fetchUserSnippets(userId));
     }
-  }
-}
+  };
+};
 
 const mapState = (state) => {
   return {
-    snippets: state.snippets
-  }
-}
+    snippets: state.snippets,
+    userId: 2
+  };
+};
 
-export default connect(mapState, mapDispatch)(Snippets)
+export default connect(mapState, mapDispatch)(Snippets);
