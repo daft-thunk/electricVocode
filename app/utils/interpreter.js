c503c61f84077a02507e6c0eae323ae39962c491
Dave Cohen <dcohen18@gmail.com>
2018-03-22 16:37:39 -0500

    Fix lowercase bug

--- app/utils/interpreter.js
+++ app/utils/interpreter.js
@@ -1,44 +1,42 @@
-import { component, store, reducer, express, stateless } from './templates'
-import electron from 'electron'
+import { component, store, reducer, express, stateless } from './templates';
+import electron from 'electron';
 
 export const dictionary = {
-  'while': () => {
-    return `while (Josh === Salty){\nreturn tear\n}`
+  while: () => {
+    return `while (Josh === Salty){\nreturn tear\n}`;
   },
-  'for': () => {
-    return `for(let i = 0; i < array.length; i++){\n}`
+  for: () => {
+    return `for(let i = 0; i < array.length; i++){\n}`;
   },
-  'function': (input) => {
-    return `const funcName = (args) => {}`
+  function: (input) => {
+    return `const funcName = (args) => {}`;
   },
-  'string': (input) => {
-    return `"${input.slice(7)}"`
+  string: (input) => {
+    return `"${input.slice(7)}"`;
   },
-  'component': () => component,
-  'store': () => store,
-  'reducer': () => reducer,
-  'express': () => express,
-  'stateless': () => stateless
-}
+  component: () => component,
+  store: () => store,
+  reducer: () => reducer,
+  express: () => express,
+  stateless: () => stateless
+};
 
 const interpreter = (speech) => {
-  console.log(speech)
-  let commandWords = speech.split(' ').filter(word => dictionary[word.toLowerCase()] !== undefined)
-  let currCommand;
+  console.log(speech);
+  let commandWords = speech.split(' ').filter(word => dictionary[word.toLowerCase()] !== undefined);
   // while (commandWords.length) {
-    currCommand = commandWords.shift().toLowerCase();
-    if (currCommand){
-      electron.ipcRenderer.send('successCommand', currCommand)
+    if (commandWords.length){
+      // get first word:
+      let currCommand = commandWords.shift().toLowerCase();
+      electron.ipcRenderer.send('successCommand', currCommand);
       return dictionary[currCommand](speech);
     }
     else {
-      electron.ipcRenderer.send('failCommand', 'command not found')
-      console.error(`Command ${speech} not recognized`)
+      electron.ipcRenderer.send('failCommand', 'command not found');
+      console.error(`Command ${speech} not recognized`);
     }
   // }
-}
+};
 
 export default interpreter;
-
-
 
