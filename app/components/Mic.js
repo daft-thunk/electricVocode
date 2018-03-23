c611e536f464fabbe771481eaa015e6ae8f5b799
Josh <carlson.joshuaph@gmail.com>
2018-03-22 19:28:13 -0500

    messing with ipc protocals

--- app/components/Mic.js
+++ app/components/Mic.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Recorder from '../public/recorder.js';
 import axios from 'axios';
-import store from '../store';
+import store  from '../store';
 import {addOutputThunk} from '../store/decoder.js'
 import electron from 'electron'
 import { dictionary } from '../utils/interpreter'
+import recorder from '../recorder'
+import initAudio from '../audio'
+
 
 class Mic extends Component {
   constructor(props) {
@@ -14,7 +16,6 @@ class Mic extends Component {
       audio_context: null,
       recorder: null
     }
-    this.startUserMedia = this.startUserMedia.bind(this);
     this.stopRecording = this.stopRecording.bind(this);
     this.blobify = this.blobify.bind(this);
   }
@@ -30,8 +31,8 @@ class Mic extends Component {
 
   stopRecording() {
     // console.log(recorder)
-    this.state.recorder.exportWAV((blob) => {
-      // console.log(blob)
+    this.state.recorder.exportMonoWAV((blob) => {
+      console.log(blob)
       this.blobify(blob);
     });
     this.state.recorder.stop()
@@ -46,45 +47,18 @@ class Mic extends Component {
     })
     return parsed
   }
-
-  startUserMedia(stream) {
-    let input = this.state.audio_context.createMediaStreamSource(stream);
-
-    this.setState({recorder: new Recorder(input)}, () => {
-      electron.remote.globalShortcut.register('Alt+z', () => {
-        console.log('recording')
-        this.state.recorder.record()
-        electron.ipcRenderer.send('startRecording')
-        setTimeout(()=>{
-          this.stopRecording()
-          electron.ipcRenderer.send('stopRecording')
-        }, 4000)
-      });
-      //can force stop
-      electron.remote.globalShortcut.register('Alt+a', () =>{
-        (console.log('stopping'))
-        this.stopRecording()
-      })
-    })
-  }
-
   componentDidMount() {
-    try {
-      // webkit shim
-      window.AudioContext = window.AudioContext || window.webkitAudioContext;
-      navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
-      window.URL = window.URL || window.webkitURL;
-
-      this.setState({audio_context: new AudioContext}, () => {
-        navigator.getUserMedia({audio: true}, this.startUserMedia, function(e) {
-          // __log('No live audio input: ' + e);
-        });
-      })
-      //__log('Audio context set up.');
-      //__log('navigator.getUserMedia ' + (navigator.getUserMedia ? 'available.' : 'not present!'));
-    } catch (e) {
-      alert('No web audio support in this browser!');
-    }
+    initAudio()
+    .then(recorder => {
+      this.setState({ recorder }, () => electron.remote.globalShortcut.register('Alt+z', () => {
+              this.state.recorder.record()
+              electron.ipcRenderer.send('startRecording')
+              setTimeout(()=>{
+                this.stopRecording()
+                electron.ipcRenderer.send('stopRecording')
+              }, 4000)
+            }));
+    })
   }
 
   render() {
--- app/main.dev.js
+++ app/main.dev.js
@@ -18,6 +18,7 @@ import store from './store'
 import path from 'path'
 
 let mainWindow = null;
+let worker = null
 let tray = null;
 if (process.env.NODE_ENV === 'production') {
   const sourceMapSupport = require('source-map-support');
@@ -63,12 +64,12 @@ app.on('ready', async () => {
     await installExtensions();
   }
 
-  tray = new Tray(path.join(__dirname,'.', 'public/images/triangle-blue.png'))
+  tray = new Tray(path.join(__dirname,'..', 'triangle-blue.png'))
   ipcMain.on('startRecording', () => {
-    tray.setImage(path.join(__dirname,'.', 'public/images/triangle-red.png'))
+    tray.setImage(path.join(__dirname,'..', 'triangle-red.png'))
   })
   ipcMain.on('stopRecording', () => {
-    tray.setImage(path.join(__dirname,'.', 'public/images/triangle-blue.png'))
+    tray.setImage(path.join(__dirname,'..', 'triangle-blue.png'))
   })
   ipcMain.on('successCommand', (e, info) => {
     tray.setTitle(info)
@@ -82,6 +83,10 @@ app.on('ready', async () => {
     width: 1024,
     height: 728
   });
+
+  worker = new BrowserWindow({
+    show: false
+  })
 
   mainWindow.loadURL(`file://${__dirname}/app.html`);
 
@@ -108,3 +113,133 @@ app.on('ready', async () => {
 });
 
 
+var recLength = 0,
+  recBuffersL = [],
+  recBuffersR = [],
+  sampleRate;
+
+ipcMain.on('doWork', (e, action) => {
+  switch(action.command){
+    case 'init':
+      init(action.config);
+      break;
+    case 'record':
+      record(action.buffer);
+      break;
+    case 'exportMonoWAV':
+      exportMonoWAV(action.type, e);
+      break;
+    case 'getBuffers':
+      getBuffers(e);
+      break;
+    case 'clear':
+      clear();
+      break;
+  }
+})
+
+
+function init(config){
+  sampleRate = config.sampleRate;
+}
+
+function record(inputBuffer){
+  recBuffersL.push(inputBuffer[0]);
+  recBuffersR.push(inputBuffer[1]);
+  recLength += inputBuffer[0].length;
+}
+
+function exportMonoWAV(type){
+  var bufferL = mergeBuffers(recBuffersL, recLength);
+  var dataview = encodeWAV(bufferL, true);
+  var audioBlob = new Blob([dataview], { type: type });
+
+  e.sender.send(audioBlob);
+}
+
+function getBuffers() {
+  var buffers = [];
+  buffers.push( mergeBuffers(recBuffersL, recLength) );
+  buffers.push( mergeBuffers(recBuffersR, recLength) );
+  e.sender.send(buffers);
+}
+
+function clear(){
+  recLength = 0;
+  recBuffersL = [];
+  recBuffersR = [];
+}
+
+function mergeBuffers(recBuffers, recLength){
+  var result = new Float32Array(recLength);
+  var offset = 0;
+  for (var i = 0; i < recBuffers.length; i++){
+    result.set(recBuffers[i], offset);
+    offset += recBuffers[i].length;
+  }
+  return result;
+}
+
+function interleave(inputL, inputR){
+  var length = inputL.length + inputR.length;
+  var result = new Float32Array(length);
+
+  var index = 0,
+    inputIndex = 0;
+
+  while (index < length){
+    result[index++] = inputL[inputIndex];
+    result[index++] = inputR[inputIndex];
+    inputIndex++;
+  }
+  return result;
+}
+
+function floatTo16BitPCM(output, offset, input){
+  for (var i = 0; i < input.length; i++, offset+=2){
+    var s = Math.max(-1, Math.min(1, input[i]));
+    output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
+  }
+}
+
+function writeString(view, offset, string){
+  for (var i = 0; i < string.length; i++){
+    view.setUint8(offset + i, string.charCodeAt(i));
+  }
+}
+
+function encodeWAV(samples, mono){
+  var buffer = new ArrayBuffer(44 + samples.length * 2);
+  var view = new DataView(buffer);
+
+  /* RIFF identifier */
+  writeString(view, 0, 'RIFF');
+  /* file length */
+  view.setUint32(4, 32 + samples.length * 2, true);
+  /* RIFF type */
+  writeString(view, 8, 'WAVE');
+  /* format chunk identifier */
+  writeString(view, 12, 'fmt ');
+  /* format chunk length */
+  view.setUint32(16, 16, true);
+  /* sample format (raw) */
+  view.setUint16(20, 1, true);
+  /* channel count */
+  view.setUint16(22, mono?1:2, true);
+  /* sample rate */
+  view.setUint32(24, sampleRate, true);
+  /* byte rate (sample rate * block align) */
+  view.setUint32(28, sampleRate * 4, true);
+  /* block align (channel count * bytes per sample) */
+  view.setUint16(32, 4, true);
+  /* bits per sample */
+  view.setUint16(34, 16, true);
+  /* data chunk identifier */
+  writeString(view, 36, 'data');
+  /* data chunk length */
+  view.setUint32(40, samples.length * 2, true);
+
+  floatTo16BitPCM(view, 44, samples);
+
+  return view;
+}
