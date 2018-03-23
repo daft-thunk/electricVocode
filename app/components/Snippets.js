0b6f7e16c39f563b5d9ec486f35251a756694d75
jakeBergs <jake.bergal@gmail.com>
2018-03-23 09:52:35 -0500

    saving last commits before starting todays work

--- app/components/Snippets.js
+++ app/components/Snippets.js
@@ -3,17 +3,23 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Table, Icon, Divider, Button, Modal, Popconfirm, message } from 'antd';
 import { Test } from './Test';
-import { fetchUserSnippets } from '../store/snippets';
+import { fetchUserSnippets, removeUserSnippetConnection } from '../store/snippets';
 import SnippetView from './SnippetView';
 
 class Snippets extends Component {
   constructor() {
     super();
 
+    this.removeSnippet = this.removeSnippet.bind(this)
   }
 
   componentDidMount() {
     this.props.fetchSnippets(this.props.userId);
+  }
+
+  removeSnippet(snippetId) {
+    this.props.disconnectSnippet(this.props.userId, snippetId);
+    message.info('Snippet has been removed')
   }
 
   render() {
@@ -45,7 +51,7 @@ class Snippets extends Component {
             <Button icon="fork" />
           }
           <Divider type="vertical" />
-          <Popconfirm title={remText} onConfirm={confirm} okText="Yes" cancelText="No" >
+          <Popconfirm title={remText} onConfirm={() => this.removeSnippet(record.id)} okText="Yes" cancelText="No" >
             <Button type="danger" icon="close" />
           </Popconfirm>
         </span>
@@ -65,6 +71,9 @@ const mapDispatch = (dispatch) => {
   return {
     fetchSnippets(userId) {
       dispatch(fetchUserSnippets(userId));
+    },
+    disconnectSnippet(userId, snippetId) {
+      dispatch(removeUserSnippetConnection(userId, snippetId))
     }
   };
 };
--- app/store/snippets.js
+++ app/store/snippets.js
@@ -1,13 +1,17 @@
 import axios from 'axios';
 
 const GET_SNIPPETS = 'GET_SNIPPETS';
+const REMOVE_SNIPPET = 'REMOVE_SNIPPET';
 
 const getSnippets = (snippets) => ({ type: GET_SNIPPETS, snippets });
+const removeSnippet = (snippetId) => ({type: REMOVE_SNIPPET, snippetId})
 
 export default function (state = [], action) {
   switch (action.type) {
     case GET_SNIPPETS:
       return action.snippets;
+    case REMOVE_SNIPPET:
+      return state.filter(snippet => snippet.id !== action.snippetId);
     default:
       return state;
   }
@@ -21,3 +25,13 @@ export const fetchUserSnippets = (userId) => dispatch => {
     })
     .catch(console.error)
 };
+
+export const removeUserSnippetConnection = (userId, snippetId) => dispatch => {
+  axios.delete(`http://localhost:8080/api/users/${userId}/snippet/${snippetId}`)
+    .then(res => res.status)
+    .then(status => {
+      console.log(status);
+      dispatch(removeSnippet(snippetId))
+    })
+    .catch(console.error)
+}
