9acee13e4d82b26eafb8f173bbdf11319eb701dd
Kevin Wuerdeman <kevinwuerdeman@gmail.com>
2018-03-25 11:54:45 -0500

    snip store

--- app/store/snippets.js
+++ app/store/snippets.js
@@ -4,10 +4,12 @@ import serverUrl from '../utils/serverUrl';
 const GET_SNIPPETS = 'GET_SNIPPETS';
 const REMOVE_SNIPPET = 'REMOVE_SNIPPET';
 const ADD_SNIPPET = 'ADD_SNIPPET';
+const ADD_SNIPPET_CONNECTION = 'ADD_SNIPPET_CONNECTION'
 
 const getSnippets = (snippets) => ({ type: GET_SNIPPETS, snippets });
-const removeSnippet = (snippetId) => ({type: REMOVE_SNIPPET, snippetId})
+const removeSnippet = (snippetId) => ({type: REMOVE_SNIPPET, snippetId});
 const addSnippet = (snippet) => ({ type: ADD_SNIPPET, snippet });
+const addSnippetConnection = (snippet) => ({type: ADD_SNIPPET_CONNECTION, snippet});
 
 export default function (state = [], action) {
   switch (action.type) {
@@ -17,6 +19,8 @@ export default function (state = [], action) {
       return state.filter(snippet => snippet.id !== action.snippetId);
     case ADD_SNIPPET:
       return [action.snippet, ...state];
+    case ADD_SNIPPET_CONNECTION:
+      return []
     default:
       return state;
   }
@@ -50,5 +54,14 @@ export const removeUserSnippetConnection = (userId, snippetId) => dispatch => {
       dispatch(removeSnippet(snippetId))
     })
     .catch(console.error)
-}
+};
 
+export const addUserSnippetConnection = (userId, snippetId) => dispatch => {
+  axios.delete(`http://localhost:8080/api/users/${userId}/snippet/${snippetId}`)
+    .then(res => res.status)
+    .then(status => {
+      console.log(status);
+      dispatch(addSnippetConnection(snippetId))
+    })
+    .catch(console.error)
+};
