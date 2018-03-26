d47b76b1a96b52e86fdba932c55c194edda51a55
Dave Cohen <dcohen18@gmail.com>
2018-03-26 17:17:41 -0500

    Add docs routes

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,10 @@
 
 import React from 'react';
 import { Switch, Route } from 'react-router';
-import { Login, App, Snippets, Signup, Discover, Profile, VoiceCommands } from './components';
+import { Login, App, Snippets, Signup, Discover, Profile, VoiceCommands, KeyboardShortcuts } from './components';
 import Test from './components/Test';
 import AppContainer from './containers/App';
-import HomePage from './containers/HomePage';
+// import HomePage from './containers/HomePage';
 import webView from './components/webView';
 
 export default () => (
@@ -18,6 +18,7 @@ export default () => (
       <Route path="/discover" component={Discover} />
       <Route path="/profile" component={Profile} />
       <Route path="/docs/voice-commands" component={VoiceCommands} />
+      <Route path="/docs/keyboard-shortcuts" component={KeyboardShortcuts} />
       <Route path="/" component={Login} />
     </Switch>
   </AppContainer>
