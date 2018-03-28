61c82edb41b45867c0fe23ec227048b7f8b998be
Dave Cohen <dcohen18@gmail.com>
2018-03-28 11:58:02 -0500

    Remove cruft

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 import { Login, App, Snippets, Signup, Discover, Profile, VoiceCommands, KeyboardShortcuts } from './components';
-import Test from './components/Test';
+// import Test from './components/Test';
 import AppContainer from './containers/App';
 // import HomePage from './containers/HomePage';
 import webView from './components/webView';
@@ -12,7 +12,6 @@ export default () => (
     <Switch>
       <Route path="/webView/:address" component={webView} />
       <Route path="/snippets" component={Snippets} />
-      <Route path="/test" component={Test} />
       <Route path="/main" component={App} />
       <Route path="/signup" component={Signup} />
       <Route path="/discover" component={Discover} />
@@ -23,3 +22,8 @@ export default () => (
     </Switch>
   </AppContainer>
 );
+
+    //   {/*MIC IS MOUNTED HERE!*/}
+    //   <div style={{ width: 0 }}>
+    //   <Mic />
+    // </div>
